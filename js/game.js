80cbf175ecae3f27dc2a84d656ac79e7c54ed258
armcan-2525 <armcan2525@gmail.com>
2026-02-09 01:04:45 +0700

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,10 @@
+let score = 0;
+
+function startGame() {
+  document.querySelector('.game').classList.remove('hidden');
+  alert('เริ่มเกมแล้ว 🎮');
+}
+
+function changeProfile() {
+  alert('เปลี่ยนโปรไฟล์ (demo)');
+}
